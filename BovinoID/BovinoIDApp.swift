0a22e595f719e8b105337d7e993102fffef83d8c
Lukezin03 <117870645+Lukezin03@users.noreply.github.com>
2024-02-27 14:22:48 -0300

    Initial Commit

---
+++ BovinoID/BovinoIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  BovinoIDApp.swift
+//  BovinoID
+//
+//  Created by Lucas Nascimento on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BovinoIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
